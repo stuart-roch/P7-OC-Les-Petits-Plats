8cf2754a31969ad3f434de9d85a95ee1cfc0305b
Stuart ROCH <stuart.roch@hotmail.fr>
2023-02-07 20:30:57 +0100

    integation de la fonctionnalite de recherche ( prog fonctionnelle )

---
+++ js/utils/search.js
@@ -0,0 +1,52 @@
+const searchBar=document.querySelector("#search-bar-recipes");
+const recipesSection=document.querySelector(".section-recipes .row");
+
+function searchRecipes(recipes,input){
+    input=input.toLowerCase();
+    recipesSearched=recipes.filter(recipe => recipe.name.toLowerCase().includes(input) || containsIngredients(recipe.ingredients,input) || recipe.description.toLowerCase().includes(input));
+    return recipesSearched;
+}
+
+function containsIngredients(ingredients,input){
+    let contains=false;
+    ingredients.forEach(ingredient => {
+        contains=ingredient.ingredient.toLowerCase().includes(input);
+        //console.log(ingredient.ingredient,contains,input);
+    });
+    return contains;
+}
+
+function getArrayAllIngredients(recipes){
+    let arrayIngredients=[];
+    recipes.forEach(recipe => arrayIngredients.push(...recipe.ingredients.map(ingredient => ingredient.ingredient)));
+    arrayIngredients=[...new Set(arrayIngredients)];
+    return arrayIngredients;
+}
+
+function getArrayAllAppliances(recipes){
+    let arrayAppliances=[];
+    recipes.forEach(recipe => arrayIngredients.push(recipe.appliance));
+    arrayAppliances=[...new Set(arrayAppliances)];
+    return arrayAppliances;
+}
+
+function getArrayAllUtensils(recipes){
+    let arrayUtensils=[];
+    recipes.forEach(recipe => arrayIngredients.push(...recipe.ustensils));
+    arrayUtensils=[...new Set(arrayUtensils)];
+    return arrayUtensils;
+}
+
+searchBar.addEventListener("input",function(e){
+    if(searchBar.value.length >=3){
+        const searchedRecipes=searchRecipes(recipes,searchBar.value);
+        if(searchedRecipes !== undefined){
+            getArrayAllIngredients(searchedRecipes);
+            recipesSection.innerHTML="";
+            displayRecipes(searchedRecipes);
+        }
+    }else{
+        recipesSection.innerHTML="";
+        displayRecipes(recipes);
+    }
+})
