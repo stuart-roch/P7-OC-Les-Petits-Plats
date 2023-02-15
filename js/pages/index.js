69c085f189d13883e483c8f3467b6106b85d8ec8
Stuart ROCH <stuart.roch@hotmail.fr>
2023-02-15 12:37:25 +0100

    affichage des options grace a l'event focus

--- js/pages/index.js
+++ js/pages/index.js
@@ -72,20 +72,19 @@ function displayFilterOptions(recipes){
 function expandOptions(){
     const filterContainer=document.querySelector(".filter-container .row");
     const inputFilterContainer=document.querySelectorAll(".input-container");
-    inputFilterContainer.forEach(input => input.addEventListener("click",function(e){
+    inputFilterContainer.forEach(input => input.firstElementChild.addEventListener("focus",function(e){
         const optionContainer=input.nextElementSibling;
         optionContainer.classList.toggle("hidden");
-        if(input.parentElement.dataset.expanded === "true"){
+        /*if(input.parentElement.dataset.expanded === "true"){
             input.lastElementChild.classList.replace("fa-chevron-up","fa-chevron-down");
             input.parentElement.classList.replace("col-12","col-3");
             input.parentElement.classList.replace("col-lg-7","col-lg-2");
             input.parentElement.dataset.expanded="false";
-        }else{
-            input.lastElementChild.classList.replace("fa-chevron-down","fa-chevron-up");
-            input.parentElement.classList.replace("col-3","col-12");
-            input.parentElement.classList.replace("col-lg-2","col-lg-7");
-            input.parentElement.dataset.expanded="true";
-        }
+        }else{*/
+        input.lastElementChild.classList.replace("fa-chevron-down","fa-chevron-up");
+        input.parentElement.classList.replace("col-3","col-12");
+        input.parentElement.classList.replace("col-lg-2","col-lg-7");
+        input.parentElement.dataset.expanded="true";
         Array.from(filterContainer.children).forEach(child => {
             if(child !== input.parentElement && child.dataset.expanded === "true"){
                 child.lastElementChild.classList.toggle("hidden");
@@ -95,6 +94,15 @@ function expandOptions(){
                 child.dataset.expanded="false";
             }
         })
+    }))
+
+    inputFilterContainer.forEach(input => input.firstElementChild.addEventListener("blur",function(e){
+        const optionContainer=input.nextElementSibling;
+        optionContainer.classList.toggle("hidden");
+        input.lastElementChild.classList.replace("fa-chevron-up","fa-chevron-down");
+        input.parentElement.classList.replace("col-12","col-3");
+        input.parentElement.classList.replace("col-lg-7","col-lg-2");
+        input.parentElement.dataset.expanded="false";
     }))
 }
 
